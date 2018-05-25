7c263803328de324434aadb4cca616431e988e0b
Михаил Гусаров <vgg639@gmail.com>
2018-05-25 11:44:16 +0300

    функция по блоку польза для детей и взрослых

--- js/scriptJS.js
+++ js/scriptJS.js
@@ -1,7 +1,7 @@
 "use strict"
-var class_label = "good_for_people__child_or_man";
-var class_content = "good_for_people__content";
-
+/*Функция для блока "Польза для детей/польза для взрослых
+при желании можно добавить сколько угодно блоков*/
+function change_in_good_for_people(class_label,class_content){
 //Делаем начальную позицию для элементов
 function set_start_position(class_label, class_content) {
     document.querySelector('.' + class_label + ' span').className += " active";
@@ -10,69 +10,48 @@ function set_start_position(class_label, class_content) {
         elements_content[i].setAttribute("style", "display:none;");
     }
 }
-
-set_start_position(class_label, class_content);
-
 //функция, которая меняет активность
 function change_active() {
     //проверка на активность
-    if(this.className=="active"){
-       return false;
+    if (this.className == "active") {
+        return false;
     }
     //меняем активность заголовка
     function change_active_label(class_click) {
         document.getElementsByClassName("active")[0].classList.remove("active");
         class_click.classList.add("active");
     }
-    //показываем активное содержимое
-    function change_active_content(class_click) {
-        // я устал и хочу спать. Затра допишу
+    //ищем номер активного элемента
+    function get_active_number() {
+        for (i = 0; i < document.querySelectorAll('.' + class_label + ' span').length; i++) {
+            if (document.querySelectorAll('.' + class_label + ' span')[i].className == "active") {
+                return i
+            }
+        }
     }
+    //показываем активное содержимое
+    function change_active_content() {
+        var elements_content = document.getElementsByClassName(class_content);
+        for (var i = 0; i < elements_content.length; i++) {
+            if(i==get_active_number()){
+                elements_content[i].setAttribute("style", "");
+            }
+            else{
+                elements_content[i].setAttribute("style", "display:none;");
+            }
+            
+        }
+    }
+    
     change_active_label(this);
+    change_active_content();
 };
-
+set_start_position(class_label, class_content);
+//действие клика
 for (var i = 0; i < document.querySelectorAll('.' + class_label + ' span').length; i++) {
     document.querySelectorAll('.' + class_label + ' span')[i].addEventListener("click", change_active);
 };
 
-
-/*
-$(".good_for_people__child_or_man span").on('click', function (e) {
-var index = $(this).index();
-var current = $(this)
-console.log(index)
-$('.good_for_people__child_or_man span').removeClass('active');
-current.addClass('active');
-$('.good_for_people__child_or_man').css('display', 'none');
-$('.good_for_people__child_or_man').removeClass('actives');
-$('.good_for_people__child_or_man:eq(' + index + ')').addClass('actives');
-$('.good_for_people__child_or_man:eq(' + index + ')').fadeIn(700);
-})*/
-
-
-/*
-Версия Мишани
-
-var cha = document.querySelectorAll('.good_for_people__child_or_man span');
-
-//Проходимся по всем span 
-for (var i=0; i<cha.length;i++){
-//Слушаем событие клика
-    cha[i].addEventListener("click",function(){
-    //Если класс уже имеется прерываем функции
-        if(this.classList.contains('active')){
-            return false;
-        }
-        //Добавляем класс active
-        this.classList.add("active");
-        for (var j=0; j<cha.length; j++){
-        //Удаляем у остальных класс active
-            if(cha[j]!=this){
-                cha[j].classList.remove('active');
-            }
-        }
-        // ....PROFIT!
-    });
 }
 
-*/
+change_in_good_for_people("good_for_people__child_or_man","good_for_people__content");
