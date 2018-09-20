46e76aa4c7473af60ac0d287fd98e4d7c931a85b
Михаил Гусаров <vgg639@gmail.com>
2018-09-20 22:16:31 +0300

    Revert "Revert "удаляем функции к собачим""
    This reverts commit 4f5fe2afb93f3eab3b311f8d92134470a043fe4c.

--- js/scriptJS.js
+++ js/scriptJS.js
@@ -1,63 +1,57 @@
-
 /*Функция для блока "Польза для детей/польза для взрослых
 при желании можно добавить сколько угодно блоков*/
-(function (){
+(function () {
     "use strict"
-    var class_label= "good_for_people__child_or_man",
-        class_content="good_for_people__content";
-   
-    
-//Делаем начальную позицию для элементов
-function set_start_position(class_label, class_content) {
+    var class_label = "good_for_people__child_or_man",
+        class_content = "good_for_people__content";
+
+
+    //Делаем начальную позицию для элементов
+
     document.querySelector('.' + class_label + ' span').classList.add("active");
-    var elements_content = document.querySelectorAll('.'+class_content);
+    var elements_content = document.querySelectorAll('.' + class_content);
     for (var i = 1; i < elements_content.length; i++) {
-        elements_content[i].style.display='none';
+        elements_content[i].style.display = 'none';
     }
-}
-//функция, которая меняет активность
-function change_active() {
-    //проверка на активность
-    if (this.classList.contains("active")) {
-        return false;
-    }
-    //меняем активность заголовка
-    function change_active_label(class_click) {
+
+    //функция, которая меняет активность
+    function change_active() {
+        //проверка на активность
+        if (this.classList.contains("active")) {
+            return false;
+        }
+        //меняем активность заголовка
+
         document.querySelectorAll(".active")[0].classList.remove("active");
-        class_click.classList.add("active");
-    }
-    //ищем номер активного элемента
-    function get_active_number() {
+        this.classList.add("active");
+
+        //ищем номер активного элемента
+
         for (i = 0; i < document.querySelectorAll('.' + class_label + ' span').length; i++) {
             if (document.querySelectorAll('.' + class_label + ' span')[i].className == "active") {
                 return i
             }
         }
-    }
-    //показываем активное содержимое
-    function change_active_content() {
-        var elements_content = document.querySelectorAll('.'+class_content);
-        for (var i = 0; i < elements_content.length; i++) {
-            if(i==get_active_number()){
-                elements_content[i].style.display=null;
-            }
-            else{
-                elements_content[i].style.display='none';
-            }
-            
-        }
-    }
-    
-    change_active_label(this);
-    change_active_content();
-};
-set_start_position(class_label, class_content);
-//действие клика
-for (var i = 0; i < document.querySelectorAll('.' + class_label + ' span').length; i++) {
-    document.querySelectorAll('.' + class_label + ' span')[i].addEventListener("click", change_active);
-};
 
-})(document,window)
+        //показываем активное содержимое
+
+        var elements_content = document.querySelectorAll('.' + class_content);
+        for (var i = 0; i < elements_content.length; i++) {
+            if (i == get_active_number()) {
+                elements_content[i].style.display = null;
+            } else {
+                elements_content[i].style.display = 'none';
+            }
+
+        }
+
+    };
+    //действие клика
+    for (var i = 0; i < document.querySelectorAll('.' + class_label + ' span').length; i++) {
+        document.querySelectorAll('.' + class_label + ' span')[i].addEventListener("click", change_active);
+    };
+
+})(document, window)
 
 
 
